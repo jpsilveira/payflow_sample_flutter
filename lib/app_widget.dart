e47fb7dffa2d59bdbcb4665ffa14719f82d072f5
jpsilveira <bitsoft.jpsilveira@gmail.com>
2021-07-07 22:38:39 -0300

    Add buttons on home page wip

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow_flutter/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow_flutter/modules/home/home_page.dart';
 import 'package:payflow_flutter/modules/splash/splash_page.dart';
 
@@ -13,6 +14,7 @@ class AppWidget extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Pay Flow',
       theme: ThemeData(
+        primarySwatch: Colors.orange,
         primaryColor: AppColors.primary,
       ),
       initialRoute: "/splash",
@@ -20,6 +22,7 @@ class AppWidget extends StatelessWidget {
         "/splash": (context) => SplashPage(),
         "/home": (context) => HomePage(),
         "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
       }, //SplashPage(), //HomePage(),
     );
   }
